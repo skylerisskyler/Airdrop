48d2f8d892e14495cf76913131ec4638062145a2
Skyler Riley <riley.skyler@gmail.com>
2018-01-27 15:58:13 -0800

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,114 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+
+  init: function() {
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    // Initialize web3 and set the provider to the testRPC.
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // set the provider you want from Web3.providers
+      App.web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545');
+      web3 = new Web3(App.web3Provider);
+    }
+
+    return App.initContract();
+  },
+
+  initContract: function() {
+    $.getJSON('TutorialToken.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract.
+      var TutorialTokenArtifact = data;
+      App.contracts.TutorialToken = TruffleContract(TutorialTokenArtifact);
+
+      // Set the provider for our contract.
+      App.contracts.TutorialToken.setProvider(App.web3Provider);
+
+      // Use our contract to retieve and mark the adopted pets.
+      return App.getBalances();
+    });
+
+    return App.bindEvents();
+  },
+
+  bindEvents: function() {
+    $(document).on('click', '#transferButton', App.handleTransfer);
+  },
+
+
+  handleTransfer: function(event) {
+    event.preventDefault();
+
+    var amount0 = parseInt($('#TTTransferAmount0').val());
+    var toAddress0 = $('#TTTransferAddress0').val();
+    var amount1 = parseInt($('#TTTransferAmount1').val());
+    var toAddress1 = $('#TTTransferAddress1').val();
+    var amount2 = parseInt($('#TTTransferAmount2').val());
+    var toAddress2 = $('#TTTransferAddress2').val();
+
+    var newArr = [{amount: amount0, address: toAddress0},{amount: amount1, address: toAddress1},{amount: amount2, address: toAddress2}];
+    console.log(newArr)
+    console.log('Transfer ' + amount + ' TT to ' + toAddress);
+
+    var tutorialTokenInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.TutorialToken.deployed().then(function(instance) {
+        tutorialTokenInstance = instance;
+        console.log(newArr);
+        for(var i = 0; i < newArr.length; i++){
+          tutorialTokenInstance.transfer(newArr[i].address, newArr[i].amount, {from: account}).then(console.log);
+        }
+      }).then(function(result) {
+        console.log('Transfer Successful');
+        //return App.getBalances();
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  },
+
+  getBalances: function(adopters, account) {
+    console.log('Getting balances...');
+
+    var tutorialTokenInstance;
+
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+
+      var account = accounts[0];
+
+      App.contracts.TutorialToken.deployed().then(function(instance) {
+        tutorialTokenInstance = instance;
+
+        return tutorialTokenInstance.balanceOf(account);
+      }).then(function(result) {
+        balance = result.c[0];
+
+        $('#TTBalance').text(balance);
+      }).catch(function(err) {
+        console.log(err.message);
+      });
+    });
+  }
+
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
